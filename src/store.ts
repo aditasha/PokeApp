196de01502849efe183e004627cc31278294b6a4
Aditasha Fadhila <83056427+aditasha@users.noreply.github.com>
2024-02-19 09:20:26 +0700

    Update store.ts comments

--- src/store.ts
+++ src/store.ts
@@ -19,5 +19,5 @@ setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// Inferred type
 export type AppDispatch = typeof store.dispatch;
